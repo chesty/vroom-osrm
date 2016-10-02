a01961003386442ded8702be6c39765cb5c6c549
michael chesterton <michael.c@kkts.com.au>
2016-10-02 21:40:57 +1100

    remove duplicate OSM_PBF and create it based on OSM_PBF_URL

--- osrm.sh
+++ osrm.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 : ${CPUS:=1}
-: ${OSM_PBF:=/osm/planet-latest.osm.pbf}
 : ${OSM_PBF_URL:=http://mirror2.shellbot.com/osm/planet-latest.osm.pbf}
 : ${DATA_PATH:=/osm}
 : ${PROFILE:=car}
+
+
+OSM_PBF=`basename $OSM_PBF_URL`
+OSM_PBF="${DATA_PATH}/${OSM_PBF}"
 
 export CPUS OSM_PBF OSM_PBF_URL DATA_PATH PROFILE
 
